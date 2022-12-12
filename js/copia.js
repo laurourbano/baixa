8059d2128ea7fab113ce18f0d975b67585c5f894
Lauro Otavio Urbano <74082012+laurourbano@users.noreply.github.com>
2022-12-12 16:04:49 -0300

    atualizando
    tentando inserir um popover

--- js/copia.js
+++ js/copia.js
@@ -1,11 +1,17 @@
-window.onload = function() {
+window.onload = function () {
   var copyTextareaBtn = Array.prototype.slice.call(document.querySelectorAll('.botao'));
   var copyTextarea = Array.prototype.slice.call(document.querySelectorAll('.area'));
-  copyTextareaBtn.forEach(function(btn, idx) {
-    btn.addEventListener("click", function() {
+  copyTextareaBtn.forEach(function (btn, idx) {
+    btn.addEventListener("click", function () {
       copyTextarea[idx].select();
-      var msg = document.execCommand('copy')?'funcionou':'deu erro';
+      var msg = document.execCommand('copy') ? 'funcionou' : 'deu erro';
       console.log('executando para copiar texto ' + msg);
     });
   });
 }
+
+$(function () {
+  $('.popover-exemplo').popover({
+    container: 'body'
+  })
+})
