731a10ab4e9f361018f8b3408a298ba389df3f89
Lauro Otavio Urbano <74082012+laurourbano@users.noreply.github.com>
2022-12-13 10:53:55 -0300

    Update copia.js

--- js/copia.js
+++ js/copia.js
@@ -1,29 +1,21 @@
 window.onload = function () {
+
   var copyTextareaBtn = Array.prototype.slice.call(document.querySelectorAll('.botao'));
   var copyTextarea = Array.prototype.slice.call(document.querySelectorAll('.area'));
+  var copiado = Array.prototype.slice.call(document.querySelectorAll('.copiado'));
+
   copyTextareaBtn.forEach(function (btn, idx) {
     btn.addEventListener("click", function () {
+
       copyTextarea[idx].select();
       var msg = document.execCommand('copy') ? 'funcionou' : 'deu erro';
       console.log('executando para copiar texto ' + msg);
 
-      //verificar para aplicar em todos os botões
-      var copiado = Array.prototype.slice.call(document.querySelectorAll('.copiado'));
+      copiado[idx].classList.remove('invisible');
+      setTimeout(() => {
+        copiado[idx].classList.add('invisible');
+      }, 2 * 1000);
 
-      copyTextareaBtn.forEach(function (btn, idx) {
-        btn.addEventListener("click", function () {
-          copiado[idx].select();
-          var msg = document.execCommand('copy') ? 'funcionou' : 'deu erro';
-          console.log('executando para copiar texto ' + msg);
-        })})
-        /*copyTextareaBtn.forEach(function (b, i) {
-          b.addEventListener("click", function () {
-            copiado.classList[i].remove('invisible');
-            setTimeout(() => {
-              b.classList[i].add('invisible');
-            }, 5 * 1000);
-          });
-        });*/
-      });
     });
-  }
+  });
+}
