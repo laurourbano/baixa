55ee4665f0e1d41fc225cde1503df6cc396da4aa
Lauro Otavio Urbano <74082012+laurourbano@users.noreply.github.com>
2022-12-13 10:40:13 -0300

    Update copia.js

--- js/copia.js
+++ js/copia.js
@@ -6,10 +6,24 @@ window.onload = function () {
       copyTextarea[idx].select();
       var msg = document.execCommand('copy') ? 'funcionou' : 'deu erro';
       console.log('executando para copiar texto ' + msg);
-      document.querySelector('.copiado').classList.remove('invisible')
-      setTimeout(() => {
-        document.querySelector('.copiado').classList.add('invisible')
-      }, 5 * 1000);
+
+      //verificar para aplicar em todos os botões
+      var copiado = Array.prototype.slice.call(document.querySelectorAll('.copiado'));
+
+      copyTextareaBtn.forEach(function (btn, idx) {
+        btn.addEventListener("click", function () {
+          copiado[idx].select();
+          var msg = document.execCommand('copy') ? 'funcionou' : 'deu erro';
+          console.log('executando para copiar texto ' + msg);
+        })})
+        /*copyTextareaBtn.forEach(function (b, i) {
+          b.addEventListener("click", function () {
+            copiado.classList[i].remove('invisible');
+            setTimeout(() => {
+              b.classList[i].add('invisible');
+            }, 5 * 1000);
+          });
+        });*/
+      });
     });
-  });
-}
+  }
