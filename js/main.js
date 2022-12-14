4d9e971629b40b2353327a951cb574d8ae744864
Lauro Otavio Urbano <74082012+laurourbano@users.noreply.github.com>
2022-12-14 13:46:17 -0300

    atualizando o button modetheme

--- js/main.js
+++ js/main.js
@@ -1,16 +1,9 @@
 
 const lightModeClass = 'bg-light';
-console.log(lightModeClass)
 const button = document.getElementById('botao');
-console.log(button)
-const fieldset = document.getElementsByTagName('fieldset');
-console.log(fieldset)
 const body = document.getElementsByTagName('body')[0];
-console.log(body)
 const footer = document.getElementsByTagName('footer')[0];
-console.log(footer)
 const nav = document.getElementsByTagName('nav')[0];
-console.log(nav)
 
 function changeMode() {
 	changeClasses();
@@ -19,12 +12,9 @@ function changeMode() {
 
 function changeClasses() {
 	button.classList.toggle('btn-dark');
-	fieldset.forEach(function(){
-		fieldset.classList.toggle('text-dark')
-	});
 	body.classList.toggle('bg-dark');
-	footer.classList.toggle('text-dark');
-	nav.classList.toggle('text-dark');
+	footer.classList.toggle('text-light');
+	nav.classList.toggle('text-light');
 	
 }
 
@@ -34,10 +24,8 @@ function changeText() {
 
 	if (body.classList.contains(lightModeClass)) {
 		button.innerHTML = lightMode;
-		h1.innerHTML = lightMode + ' ON';
 		return;
 	}
 
 	button.innerHTML = darkMode;
-	h1.innerHTML = darkMode + ' ON';
 }
