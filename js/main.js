2a90c82b6696846de91c7b3c574435e2e6960341
Lauro Otavio Urbano <74082012+laurourbano@users.noreply.github.com>
2023-01-26 15:53:48 -0300

    corrigir toggle da aplicacao

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 
-const button = document.getElementById('botao');
+const button = document.getElementsByTagName('input')[0];
 const body = document.getElementsByTagName('body')[0];
+const fieldset = document.getElementsByTagName('fieldset');
+const legend = document.getElementsByTagName('legend');
+const textoEscuro = 'text-dark';
+const textoClaro = 'text-light';
+const botaoEscuro = 'btn-dark';
+const fundoEscuro = 'bg-dark';
+const fundoClaro = 'bg-light';
+const botaoClaro = 'btn-light';
 
 function changeMode() {
 	changeClasses();
@@ -8,19 +16,20 @@ function changeMode() {
 }
 
 function changeClasses() {
-	button.classList.toggle('btn-dark');
-	body.classList.toggle('bg-dark');
-	body.classList.toggle('text-dark');
+	button.classList.toggle(fundoEscuro, botaoEscuro );
+	body.classList.toggle(fundoEscuro);
 }
 
 function changeText() {
-	const lightMode = 'text-light';
-	const darkMode = 'text-dark';
+	const lightMode = textoClaro;
+	const darkMode = textoEscuro;
 
-	if (body.classList.contains('bg-light')) {
-		button.innerHTML = 'bg-light';
+	if (body.classList.contains(fundoClaro)) { 
+		button.innerHTML = fundoClaro;
 		return;
 	}
 
 	button.innerHTML = darkMode;
+	body.classList.toggle(textoEscuro);
+
 }
